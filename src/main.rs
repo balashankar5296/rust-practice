6c8029f824666712531e5c9f89476133bf12cdd8
balashankar <balashankar.mony@unitsec.com>
2025-02-17 13:55:16 +0530

    fixed arithmetic operation

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,46 @@
 use std::io;
 fn main() {
     println!("Enter the first operand");
-    let input1:i32=io::stdin().lines().next().unwrap().unwrap().parse().unwrap();
+    let input1: i32 = io::stdin()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse()
+        .unwrap();
     println!("Enter the second operand");
-    let input2:i32=io::stdin().lines().next().unwrap().unwrap().parse().unwrap();
+    let input2: i32 = io::stdin()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse()
+        .unwrap();
     println!("Enter the operation you want to perform");
-    let sum:i32;
-    let mut operator:String=String::new();
-    io::stdin().read_line(&mut operator).ok().expect("failed to read line");
+    let result: i32;
+    let mut operator: String = String::new();
+    io::stdin()
+        .read_line(&mut operator)
+        .ok()
+        .expect("failed to read line");
+    operator=operator.trim().to_ascii_lowercase();
 
-    match &operator{
-        addition=>{println!("{}",input1+input2)},
-        subtraction=> {println!("{}", input1-input2)},
-        multiplication=>{
-                        println!("{}",input1*input2);
-                        }
+     match operator.as_str() {
+        "addition" => {
+            result = input1 + input2;
+            println!("{}", result);
+        }
+        "subtraction" => {
+            let result = input1 + input2;
+            println!("{}", result);
+        }
+        "multiplication" => {
+            let result = input1 + input2;
+            println!("{}", result);
+        }
+        _ =>{
+            println!("operation can't be performed");
+        }
     }
-    
-    
-  
-
 
 }
