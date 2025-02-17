8863fb8907ff5327f8c9834d8810490c40c1ab2d
balashankar.mony@unitsec.com+1-at-851725351322 <balashankar.mony@unitsec.com+1-at-851725351322>
2025-02-17 07:13:53 +0530

    trying switch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the first operand");
+    let input1:i32=io::stdin().lines().next().unwrap().unwrap().parse().unwrap();
+    println!("Enter the second operand");
+    let input2:i32=io::stdin().lines().next().unwrap().unwrap().parse().unwrap();
+    println!("Enter the operation you want to perform");
+    let sum:i32;
+    let mut operator:String=String::new();
+    io::stdin().read_line(&mut operator).ok().expect("failed to read line");
+
+    match &operator{
+        addition=>{println!("{}",input1+input2)},
+        subtraction=> {println!("{}", input1-input2)},
+        multiplication=>{
+                        println!("{}",input1*input2);
+                        }
+    }
+    
+    
+  
+
+
 }
